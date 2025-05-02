1d0ccf58b77730b1a8059c68647160bcc7480f91
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-02 18:51:59 -0300

    created the Note class

---
+++ lib/model/note.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+
+class Note {
+  String _name = '';
+  String _description = '';
+
+  late final TextEditingController _nameController;
+  late final TextEditingController _descriptionController;
+
+  String get name => _name;
+
+  String get description => _description;
+
+  TextEditingController get nameController => _nameController;
+
+  TextEditingController get descriptionController => _descriptionController;
+
+  void changeDescription() {
+    _description = _descriptionController.text;
+  }
+
+  void changeName() {
+    _name = _nameController.text;
+  }
+}
