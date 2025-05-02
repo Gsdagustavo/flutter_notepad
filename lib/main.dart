85741cf964cd1ad1bc0922e9b101b218529a37cf
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-02 18:44:18 -0300

    reformulated the homepage and made an appbar to handle the theme toggling feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(
@@ -18,14 +19,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<ThemeState>(
       builder:
-          (context, themeState, _) => MaterialApp(
+          (context, state, child) => MaterialApp(
             title: 'Notepad',
-
-            theme: themeState.lightTheme ? ThemeData.light() : ThemeData.dark(),
             debugShowCheckedModeBanner: false,
-            routes: {'/': (context) => const HomePage()},
 
-            initialRoute: '/',
+            /// sets the theme mode based on the current ThemeState
+            themeMode: state.themeMode,
+
+            /// sets the light theme
+            theme: ThemeData(
+              brightness: Brightness.light,
+              primarySwatch: Colors.blue,
+            ),
+
+            /// sets the dark theme
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+              primarySwatch: Colors.blue,
+            ),
+
+            home: const HomePage(),
           ),
     );
   }
@@ -39,28 +52,62 @@ class HomePage extends StatelessWidget {
     return Consumer<ThemeState>(
       builder:
           (context, themeState, _) => Scaffold(
-            body: Center(
-              child: IconButton(
-                onPressed: () => themeState.toggleTheme(),
-                icon: Icon(
-                  themeState.lightTheme ? Icons.light_mode : Icons.dark_mode,
-                ),
-              ),
-            ),
+            appBar: const MyAppBar(),
+
+            body: Center(child: Text('Hello World!')),
           ),
     );
   }
 }
 
-class ThemeState with ChangeNotifier {
-  bool _lightTheme = true;
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({super.key});
 
-  bool get lightTheme => _lightTheme;
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Consumer<ThemeState>(
+      builder:
+          (context, state, child) => AppBar(
+            title: Text('Notepad'),
+            centerTitle: true,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: IconButton(
+                  onPressed: () => state.toggleTheme(),
+                  icon: Icon(
+                    state.isLightMode ? Icons.light_mode : Icons.dark_mode,
+                  ),
+                ),
+              ),
+            ],
+          ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+/// keeps track of the current Theme State
+class ThemeState with ChangeNotifier {
+  static const String appThemeModeKey = 'appThemeMode';
+
+  /// default theme is light
+  ThemeMode _themeMode = ThemeMode.light;
+
+  ThemeMode get themeMode => _themeMode;
+
+  bool get isLightMode => _themeMode == ThemeMode.light;
+
+  late final SharedPreferences _sharedPreferences;
 
   /// toggles the current theme (whether is light or dark)
   void toggleTheme() {
-    _lightTheme = !_lightTheme;
-    debugPrint(_lightTheme.toString());
+    _themeMode =
+        _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
     notifyListeners();
   }
 }
